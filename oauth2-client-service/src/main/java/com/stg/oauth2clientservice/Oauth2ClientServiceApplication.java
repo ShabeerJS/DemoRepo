805a0bc10da317d9cf5c62a7164829e758aa38d4
Shabeer Jaffer <shabeerj@stg.com>
2023-02-02 12:25:53 +0530

    first commit

---
+++ oauth2-client-service/src/main/java/com/stg/oauth2clientservice/Oauth2ClientServiceApplication.java
@@ -0,0 +1,14 @@
+package com.stg.oauth2clientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("he");
+		SpringApplication.run(Oauth2ClientServiceApplication.class, args);
+	}
+
+}
